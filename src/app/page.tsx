00799af4678923c896838b9370e2cf06ba6a1e49
razaahmad11 <razaqazhi@gmail.com>
2024-01-31 19:04:47 +0500

    fix: text-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Herosection from "@/component/herosection";
 import Images from "@/component/images";
 import Realword from "@/component/realword";
+import Text from "@/component/text";
 import Vanlife from "@/component/vanlife";
 import { Box } from "@mui/material";
 export default function HomePage() {
@@ -11,6 +12,7 @@ export default function HomePage() {
       <Images />
       <Realword />
       <Vanlife />
+      <Text />
     </Box>
   );
 }
