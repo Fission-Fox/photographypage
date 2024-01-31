71c1eacf158514717ac8fe3b440051736f14271e
aimanfatima248 <aimanfatima65@outlook.com>
2024-01-31 20:28:20 +0500

    fix: mystorysection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
+import Photographer from "@/components/photographer";
 import { Box } from "@mui/material";
 export default function HomePage() {
 
   return (
     <Box sx={{ width: "100%" }}>
-      homepage
+      <Photographer />
     </Box>
   );
 }
