886ed2e58a8d509cde088996d8b70d5fbe4c9a0a
razaahmad11 <razaqazhi@gmail.com>
2024-01-31 17:17:06 +0500

    fix: images section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Herosection from "@/component/herosection";
+import Images from "@/component/images";
 import { Box } from "@mui/material";
 export default function HomePage() {
   return (
     <Box>
       <Herosection />
+      <Images />
     </Box>
   );
 }
