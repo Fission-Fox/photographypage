e7323baeaf6be47eb8ce135c8d5670088aea347b
Mriakhan <mria.khan123@gmail.com>
2024-02-01 15:07:32 +0500

    fix: portfolio

--- src/components/love.tsx
+++ src/components/love.tsx
@@ -17,15 +17,51 @@ export default function Love() {
         </div>
         <div className="  bg-[#FAFAFA] text-[#3C3C3CB3] w-[628px] h-[220] sm:h-[420px] flex justify-center m-auto px-3 sm:px-0">
           <div>
-            <h2 className="text-[30px] mt-6 mb-12 md:mb-14 "> Love</h2>
-            <p className="mb-16 text-[12px] sm:text-[16px] 2xl:text-[20px]">
+            <h2
+              className="text-[30px] mt-6 mb-12 md:mb-14 "
+              style={{
+                fontFamily: "Sorts Mill Goudy",
+                fontSize: "1.375rem",
+                letterSpacing: "0.01em",
+                wordSpacing: "0em",
+                lineHeight: "1.3em",
+                textTransform: "none",
+                fontWeight: "normal",
+              }}
+            >
+              {" "}
+              Love
+            </h2>
+            <p
+              className="mb-16 text-[12px] sm:text-[16px] 2xl:text-[20px]"
+              style={{
+                fontFamily: "Overpass, sans-serif;",
+                fontSize: "0.75rem",
+                letterSpacing: "0em",
+                wordSpacing: "0em",
+                lineHeight: " 1.73em",
+                textTransform: "none",
+                fontWeight: "normal",
+              }}
+            >
               {" "}
               A gallery of elopement photography from weddings, love <br />
               and marriage. “Your task is not to seek for love, but <br />{" "}
               merely to s...
             </p>
             <div>
-              <button className=" border text-[#3C3C3C] hover:bg-[black] hover:text-white w-56 h-14 mb-16">
+              <button
+                className=" border text-[#3C3C3C] hover:bg-[black] hover:text-white w-56 h-14 mb-16"
+                style={{
+                  fontFamily: "Overpass, sans-serif;",
+                  fontSize: "0.6875rem",
+                  letterSpacing: "0.21em",
+                  wordSpacing: "0em",
+                  lineHeight: "  1.87em",
+                  textTransform: "uppercase",
+                  fontWeight: "normal",
+                }}
+              >
                 {" "}
                 READ MORE{" "}
               </button>
@@ -36,15 +72,51 @@ export default function Love() {
       <div className="flex flex-wrap  w-[90%] xl:w-[1257px]  justify-center  items-center m-auto ">
         <div className="  bg-[#FAFAFA] text-[#3C3C3CB3] w-[628px] h-[220] sm:h-[420px] flex justify-center m-auto px-3 sm:px-0">
           <div>
-            <h2 className="text-[30px] mt-6 mb-12 md:mb-14 "> Adventures</h2>
-            <p className="mb-16 text-[12px] sm:text-[16px] 2xl:text-[20px]">
+            <h2
+              className="text-[30px] mt-6 mb-12 md:mb-14 "
+              style={{
+                fontFamily: "Sorts Mill Goudy",
+                fontSize: "1.375rem",
+                letterSpacing: "0.01em",
+                wordSpacing: "0em",
+                lineHeight: "1.3em",
+                textTransform: "none",
+                fontWeight: "normal",
+              }}
+            >
+              {" "}
+              Adventures
+            </h2>
+            <p
+              className="mb-16 text-[12px] sm:text-[16px] 2xl:text-[20px]"
+              style={{
+                fontFamily: "Overpass, sans-serif;",
+                fontSize: "0.75rem",
+                letterSpacing: "0em",
+                wordSpacing: "0em",
+                lineHeight: " 1.73em",
+                textTransform: "none",
+                fontWeight: "normal",
+              }}
+            >
               {" "}
               A gallery of elopement photography from weddings, love <br />
               and marriage. “Your task is not to seek for love, but <br />{" "}
               merely to s...
             </p>
             <div>
-              <button className=" border text-[#3C3C3C] hover:bg-[black] hover:text-white w-56 h-14 mb-16">
+              <button
+                className=" border text-[#3C3C3C] hover:bg-[black] hover:text-white w-56 h-14 mb-16"
+                style={{
+                  fontFamily: "Overpass, sans-serif;",
+                  fontSize: "0.6875rem",
+                  letterSpacing: "0.21em",
+                  wordSpacing: "0em",
+                  lineHeight: "  1.87em",
+                  textTransform: "uppercase",
+                  fontWeight: "normal",
+                }}
+              >
                 {" "}
                 READ MORE{" "}
               </button>
@@ -71,15 +143,51 @@ export default function Love() {
         </div>
         <div className="  bg-[#FAFAFA] text-[#3C3C3CB3] w-[628px] h-[220] sm:h-[420px] flex justify-center m-auto px-3 sm:px-0">
           <div>
-            <h2 className="text-[30px] mt-6 mb-12 md:mb-14 "> Portraits</h2>
-            <p className="mb-16 text-[12px] sm:text-[16px] 2xl:text-[20px]">
+            <h2
+              className="text-[30px] mt-6 mb-12 md:mb-14 "
+              style={{
+                fontFamily: "Sorts Mill Goudy",
+                fontSize: "1.375rem",
+                letterSpacing: "0.01em",
+                wordSpacing: "0em",
+                lineHeight: "1.3em",
+                textTransform: "none",
+                fontWeight: "normal",
+              }}
+            >
+              {" "}
+              Portraits
+            </h2>
+            <p
+              className="mb-16 text-[12px] sm:text-[16px] 2xl:text-[20px]"
+              style={{
+                fontFamily: "Overpass, sans-serif;",
+                fontSize: "0.75rem",
+                letterSpacing: "0em",
+                wordSpacing: "0em",
+                lineHeight: " 1.73em",
+                textTransform: "none",
+                fontWeight: "normal",
+              }}
+            >
               {" "}
               A gallery of elopement photography from weddings, love <br />
               and marriage. “Your task is not to seek for love, but <br />{" "}
               merely to s...
             </p>
             <div>
-              <button className=" border text-[#3C3C3C] hover:bg-[black] hover:text-white w-56 h-14 mb-16">
+              <button
+                className=" border text-[#3C3C3C] hover:bg-[black] hover:text-white w-56 h-14 mb-16"
+                style={{
+                  fontFamily: "Overpass, sans-serif;",
+                  fontSize: "0.6875rem",
+                  letterSpacing: "0.21em",
+                  wordSpacing: "0em",
+                  lineHeight: "  1.87em",
+                  textTransform: "uppercase",
+                  fontWeight: "normal",
+                }}
+              >
                 {" "}
                 READ MORE{" "}
               </button>
